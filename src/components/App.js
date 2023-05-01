a155b3d8467aef07ca33a413c5a11f253dd41c94
Ricardo Guerra <RikkuG491@gmail.com>
2023-04-30 23:38:04 -0400

    Added more pets to the pets array

--- src/components/App.js
+++ src/components/App.js
@@ -18,13 +18,33 @@ function App() {
         id: 3,
         name: "Hammy",
         image: "./images/hamster.jpeg"
+    },
+    {
+        id: 4,
+        name: "Polly",
+        image: "./images/parrot.jpeg"
+    },
+    {
+        id: 5,
+        name: "Angel",
+        image: "./images/horse.jpeg"
+    },
+    {
+        id: 6,
+        name: "Fluffy",
+        image: "./images/guinea-pig.jpeg"
+    },
+    {
+        id: 7,
+        name: "Goldie",
+        image: "./images/fish.jpeg"
     }
-]
+  ]
 
   return (
     <div className="app">
       <Header />
-      <PetList pets={pets}/>
+      <PetList />
     </div>
   );
 }
